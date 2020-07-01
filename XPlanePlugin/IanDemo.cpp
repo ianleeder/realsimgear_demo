3c9e318e5380889a61abcb0b7b3d95bbc6baa249
Ian Leeder <i_leeder@hotmail.com>
2020-07-01 21:40:41 +1000

    Added start of loop processing.  Added a debug menu item

--- XPlanePlugin/IanDemo.cpp
+++ XPlanePlugin/IanDemo.cpp
@@ -2,11 +2,16 @@
  Borrowing from examples:
  Reload Plugins - https://developer.x-plane.com/code-sample/reloadplugins/
  Reading DataRefs - https://developer.x-plane.com/code-sample/simdata/
+ Timed Processing - https://developer.x-plane.com/code-sample/timedprocessing/
  
  API links:
  Data Refs - https://developer.x-plane.com/datarefs/
      sim/flightmodel/position/indicated_airspeed
      sim/flightmodel/position/elevation
+ 
+ Information about plugin design, and resource consumption
+ https://developer.x-plane.com/article/developing-plugins/#Guidelines_for_Plugin_Design
+ 
  */
 #if IBM
 #include <windows.h>
@@ -17,12 +22,20 @@
 #include "XPLMDataAccess.h"
 #include "XPLMPlugin.h"
 #include "XPLMMenus.h"
+#include "XPLMProcessing.h"
 
+static const float gDataRefreshRate = 0.5;
 
 static void ReloadPluginsMenuHandler(void * mRef, void * iRef);
+static float MyFlightLoopCallback(float  inElapsedSinceLastCall,
+                                  float  inElapsedTimeSinceLastFlightLoop,
+                                  int    inCounter,
+                                  void * inRefcon);
 
-static XPLMDataRef  gDataRefElevation = NULL;
-static XPLMDataRef  gDataRefAirspeed = NULL;
+static XPLMDataRef gElevation = NULL;
+static XPLMDataRef gAirspeed = NULL;
+static XPLMMenuID gPluginMenuId = NULL;
+static int gDebugMenuItemIndex = -1;
 
 PLUGIN_API int XPluginStart(
 							char *		outName,
@@ -30,27 +43,36 @@ PLUGIN_API int XPluginStart(
 							char *		outDesc)
 {
     XPLMMenuID id;
-    int item;
     
 	strcpy(outName, "IanDemo");
 	strcpy(outSig, "ianleeder.examples.iandemo");
 	strcpy(outDesc, "A demonstration of passing data between the game plugin and an Arduino compatible microcontroller.");
 	
-    item = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "IanDemo", NULL, 1);
+    int myPluginMenuIndex = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "IanDemo", NULL, 1);
 
-    id = XPLMCreateMenu("IanDemo", XPLMFindPluginsMenu(), item, ReloadPluginsMenuHandler, NULL);
-    XPLMAppendMenuItem(id, "Reload", (void *)"Reload plugins",1);
-
+    gPluginMenuId = XPLMCreateMenu("IanDemo", XPLMFindPluginsMenu(), myPluginMenuIndex, ReloadPluginsMenuHandler, NULL);
+    XPLMAppendMenuItem(gPluginMenuId, "Reload", (void *)"Reload plugins",1);
+    
+    gDebugMenuItemIndex = XPLMAppendMenuItem(gPluginMenuId, "Test2", (void *)"Reload plugins",1);
+    
+    gElevation = XPLMFindDataRef("sim/flightmodel/position/elevation");
+    gAirspeed = XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed");
+    
+    // https://developer.x-plane.com/sdk/XPLMRegisterFlightLoopCallback/
+    XPLMRegisterFlightLoopCallback(
+            MyFlightLoopCallback,    /* Callback */
+            gDataRefreshRate,        /* Interval */
+            NULL);                   /* refcon not used. */
+    
     /* We must return 1 to indicate successful initialization, otherwise we
      * will not be called back again. */
-     
     return 1;
 }
 
 PLUGIN_API void	XPluginStop(void)
 {
-    gDataRefElevation = NULL;
-    gDataRefAirspeed = NULL;
+    gElevation = NULL;
+    gAirspeed = NULL;
 }
 
 PLUGIN_API void XPluginDisable(void) { }
@@ -63,4 +85,24 @@ void ReloadPluginsMenuHandler(void * mRef, void * iRef)
     {
         XPLMReloadPlugins();
     }
+}
+
+/*
+ API on flight loop callback
+ https://developer.x-plane.com/sdk/XPLMFlightLoop_f/
+ */
+float MyFlightLoopCallback(float  inElapsedSinceLastCall,
+                           float  inElapsedTimeSinceLastFlightLoop,
+                           int    inCounter,
+                           void * inRefcon)
+{
+    float elapsed = XPLMGetElapsedTime();
+    float speed = XPLMGetDataf(gAirspeed);
+    float el = XPLMGetDataf(gElevation);
+    
+    /* Write the data to a file. */
+    //fprintf(gOutputFile, "Time=%f, lat=%f,lon=%f,el=%f.\n",elapsed, lat, lon, el);
+    
+    /* Return value indicates when we want to be called again (in seconds). */
+    return gDataRefreshRate;
 }
